88145c648d5636d74d52a8152e0382dd01475ef9
Leandro Sales <leandrossales.96@gmail.com>
2021-10-24 23:22:16 -0300

    add base home page

---
+++ lib/expenses_app.dart
@@ -0,0 +1,14 @@
+import 'package:expenses/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
