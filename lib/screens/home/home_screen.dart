64b3a700573bc616c9a2a7c9deb6b1e712cab032
Leandro Sales <leandrossales.96@gmail.com>
2021-11-11 23:50:46 -0300

    add transaction list component

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/components/transaction_list.dart';
 import 'package:expenses/models/transactions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -49,44 +50,8 @@ class HomePage extends StatelessWidget {
             child: Text('Gráfico'),
             elevation: 5,
           ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(
-                  child: Row(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 15, vertical: 10),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 2)),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      'R\$ ${tr.value.toStringAsFixed(2)}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tr.title,
-                        textAlign: TextAlign.start,
-                        style: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.date),
-                        style: const TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  )
-                ],
-              ));
-            }).toList(),
+          TransactionList(
+            transactions: _transactions,
           ),
           Card(
               elevation: 5,
