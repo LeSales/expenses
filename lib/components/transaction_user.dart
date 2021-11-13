4a3a3857a1bcf0f64f5180163e3b8c00138ed3b8
Leandro Sales <leandrossales.96@gmail.com>
2021-11-12 23:48:44 -0300

    refactor: create transaction user

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,50 @@
+import 'package:expenses/components/transaction_form.dart';
+import 'package:expenses/components/transaction_list.dart';
+import 'package:expenses/models/transactions.dart';
+import 'package:flutter/material.dart';
+
+class TransactionUser extends StatefulWidget {
+  const TransactionUser({Key? key}) : super(key: key);
+
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Fone novo',
+      value: 100.50,
+      date: DateTime.now(),
+    ),
+    /* Transaction(
+        id: 't2',
+        title: 'Calção de banho',
+        value: 30.50,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't3',
+        title: 'Lanche',
+        value: 20.50,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't4',
+        title: 'Cinema',
+        value: 56.30,
+        date: DateTime.now(),
+      ), */
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionList(transactions: _transactions),
+        const TransactionForm(),
+      ],
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,55 +1,24 @@
-import 'package:expenses/components/transaction_form.dart';
-import 'package:expenses/components/transaction_list.dart';
-import 'package:expenses/models/transactions.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final _transactions = [
-      Transaction(
-        id: 't1',
-        title: 'Fone novo',
-        value: 100.50,
-        date: DateTime.now(),
-      ),
-      /* Transaction(
-        id: 't2',
-        title: 'Calção de banho',
-        value: 30.50,
-        date: DateTime.now(),
-      ),
-      Transaction(
-        id: 't3',
-        title: 'Lanche',
-        value: 20.50,
-        date: DateTime.now(),
-      ),
-      Transaction(
-        id: 't4',
-        title: 'Cinema',
-        value: 56.30,
-        date: DateTime.now(),
-      ), */
-    ];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Despesas'),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          const Card(
+        children: const <Widget>[
+          Card(
             child: Text('Gráfico'),
             elevation: 5,
           ),
-          TransactionList(
-            transactions: _transactions,
-          ),
-          TransactionForm(),
+          TransactionUser()
         ],
       ),
     );
